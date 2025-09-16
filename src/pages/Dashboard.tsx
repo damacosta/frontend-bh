c4611a29e5b277e7718ea7200532d29afe42dbc9
Danielle Costa <d.mariano.costa@gmail.com>
2025-09-16 18:37:24 -0300

    feat: adiciona o menu lateral responsivo para mobile

---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,72 @@
+import * as React from 'react';
+import type {} from '@mui/x-date-pickers/themeAugmentation';
+import type {} from '@mui/x-charts/themeAugmentation';
+import type {} from '@mui/x-data-grid-pro/themeAugmentation';
+import type {} from '@mui/x-tree-view/themeAugmentation';
+import { alpha } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import AppNavbar from '@/components/layouts/AppNavbar';
+import MainGrid from '@/components/features/dashboard/MainGrid';
+import SideMenu from '@/components/layouts/SideMenu';
+import SideMenuMobile from '@/components/layouts/SideMenuMobile'
+import AppTheme from '@/theme/AppTheme';
+import Header from '@/components/layouts/Header';
+import {
+  chartsCustomizations,
+  dataGridCustomizations,
+  datePickersCustomizations,
+  treeViewCustomizations,
+} from '../theme/customizations';
+
+const xThemeComponents = {
+  ...chartsCustomizations,
+  ...dataGridCustomizations,
+  ...datePickersCustomizations,
+  ...treeViewCustomizations,
+};
+
+export default function Dashboard(props: { disableCustomTheme?: boolean }) {
+  const [open, setOpen] = React.useState(false);
+  const toggleDrawer = (newOpen: boolean) => () => setOpen(newOpen);
+
+  return (
+    <AppTheme {...props} themeComponents={xThemeComponents}>
+      <CssBaseline enableColorScheme />
+      <Box sx={{ display: 'flex' }}>
+        <Box sx={{ display: { xs: 'none', md: 'block' } }}>
+          <SideMenu />
+        </Box>
+
+        <AppNavbar />
+
+        <Box
+          component="main"
+          sx={(theme) => ({
+            flexGrow: 1,
+            backgroundColor: theme.vars
+              ? `rgba(${theme.vars.palette.background.defaultChannel} / 1)`
+              : alpha(theme.palette.background.default, 1),
+            overflow: 'auto',
+          })}
+        >
+          <Stack
+            spacing={2}
+            sx={{
+              alignItems: 'center',
+              mx: 3,
+              pb: 5,
+              mt: { xs: 8, md: 0 },
+            }}
+          >
+            <Header toggleDrawer={toggleDrawer} />
+            <MainGrid />
+          </Stack>
+        </Box>
+
+        <SideMenuMobile open={open} toggleDrawer={toggleDrawer} />
+      </Box>
+    </AppTheme>
+  );
+}
