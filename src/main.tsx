102074c9704f1d216a0f946fd669ad0c0cd584bc
Danielle Costa <d.mariano.costa@gmail.com>
2025-10-15 16:57:21 -0300

    feat: adicionar Auth0Provider e configuração inicial de autenticação

--- src/main.tsx
+++ src/main.tsx
@@ -1,28 +1,27 @@
-// import { MantineProvider } from "@mantine/core";
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import App from "./App";
-
-// ReactDOM.createRoot(document.getElementById("root")!).render(
-//   <React.StrictMode>
-//     <MantineProvider withGlobalStyles withNormalizeCSS>
-//       <App />
-//     </MantineProvider>
-//   </React.StrictMode>,
-// );
-
-import { ThemeProvider, CssBaseline, createTheme } from "@mui/material";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
+import { ThemeProvider, CssBaseline, createTheme } from "@mui/material";
 
 const theme = createTheme();
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>,
+    <Auth0Provider
+      domain={import.meta.env.VITE_AUTH0_DOMAIN}
+      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+      authorizationParams={{
+        redirect_uri: import.meta.env.VITE_AUTH0_REDIRECT_URI,
+      }}
+    >
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <App />
+        </ThemeProvider>
+      </BrowserRouter>
+    </Auth0Provider>
+  </React.StrictMode>
 );
