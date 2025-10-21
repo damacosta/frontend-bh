8197558189c1823623b6a795535d20bbf940b0f4
Caassapaba <jonathan.affini@gmail.com>
2025-10-21 10:49:48 -0300

    Adequação da chamada de API para compatibilidade com InfluxDB v1.8.4

--- src/api/influxdb.ts
+++ src/api/influxdb.ts
@@ -1,22 +1,19 @@
 import { InfluxDB } from '@influxdata/influxdb-client';
 
 const url = import.meta.env.VITE_INFLUX_URL;
-const token = import.meta.env.VITE_INFLUX_TOKEN;
-const org = import.meta.env.VITE_INFLUX_ORG;
+const user = import.meta.env.VITE_INFLUX_USER;
+const password = import.meta.env.VITE_INFLUX_PASSWORD;
 const bucket = import.meta.env.VITE_INFLUX_BUCKET;
+
+const token = user+":"+password;
+const org = '';
 
 const client = new InfluxDB({ url, token });
 const queryApi = client.getQueryApi(org);
 
 export const fetchSensorData = async (range: string = '-10s') => {
   const data: any[] = [];
-  const query = `
-    from(bucket: "${bucket}")
-    |> range(start: ${range})
-    |> filter(fn: (r) => r._measurement == "mqtt_consumer")
-    |> pivot(rowKey:["_time"], columnKey: ["_field"], valueColumn: "_value")
-    |> keep(columns: ["_time", "temp", "hum", "lux", "noise_db"])
-  `;
+  const query = `from(bucket: "${bucket}")|> range(start: ${range})|> filter(fn: (r) => r._measurement == "mqtt_consumer")|> pivot(rowKey:["_time"], columnKey: ["_field"], valueColumn: "_value")|> keep(columns: ["_time", "temp", "hum", "lux", "noise_db"])`;
 
   return new Promise<any[]>((resolve, reject) => {
     queryApi.queryRows(query, {
